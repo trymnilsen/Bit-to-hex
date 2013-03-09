a85fb7218b4b92bd06c12b559b133454383efc20
Trym Nilsen <trym.nilsen@gmail.com>
2013-03-09 21:24:40 +0100

    wrote converting logic

--- bittohex/bittohex/Program.cs
+++ bittohex/bittohex/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace bittohex
 {
@@ -10,6 +11,18 @@ namespace bittohex
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Selected file:" + args[0]);
+            Console.WriteLine("Output file:" + args[1]);
+            byte[] fileBytes = File.ReadAllBytes(args[0]);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in fileBytes)
+            {
+                sb.Append("0x");
+                sb.Append(Convert.ToString(b, 16));
+            }
+
+            File.WriteAllText(args[1], sb.ToString());
         }
     }
 }
